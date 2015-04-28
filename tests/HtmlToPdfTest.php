e1e1ae8e2555699d6e8244418974d4fdc1c85a41
Matthias Isler <matthias.isler@smartbridge.ch>
2015-04-28 11:25:36 +0200

    added simple unit test

--- tests/HtmlToPdfTest.php
+++ tests/HtmlToPdfTest.php
@@ -6,5 +6,9 @@ use Spiritix\HtmlToPdf\HtmlToPdf;
 
 class HtmlToPdfTest extends \PHPUnit_Framework_TestCase
 {
-    //
+    public function testInstanceCreation()
+    {
+        $htmlToPdf = new HtmlToPdf();
+        $this->assertTrue($htmlToPdf instanceof HtmlToPdf);
+    }
 }
